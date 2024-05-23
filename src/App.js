00c4ce5f8baac10a5bd5ef12e4adaf374814ece5
YUKESH R <143831307+YuksR@users.noreply.github.com>
2024-05-23 17:10:41 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> changed and committed in Github
+          Edit <code>src/App.js</code> changed and committed in Github by yukesh
         </p>
         <a
           className="App-link"
